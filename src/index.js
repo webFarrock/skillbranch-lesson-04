5aeb0a1d29244a1906120871762c45869a8d25b0
webFarrock <webFarrock@mail.ru>
2016-11-16 17:03:20 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import expressJwt from 'express-jwt';
+import jwt from 'jsonwebtoken';
+
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+   res.send('hi'); 
+});
+
+const secret = 'addfjjfjenneehdnd';
+
+app.get('/token', function(req, res){
+    const data = {
+        user: 'webfarrock',
+        name: 'some name',
+    };
+
+    return res.json(jwt.sign(data, secret));
+});
+
+app.get('/protected', expressJwt({secret}), function(req, res){
+    return res.json(req.user);
+});
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
